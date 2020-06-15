35bac5c2d52f048b935ccb2359dd019998af0981
Sergiu Voloc <SergiuVoloc@users.noreply.github.com>
2020-06-15 22:06:53 +0300

    add database connection

---
+++ Models/SVOfficeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SVOffice.Models
+{
+    public class SVOfficeDbContext : DbContext
+    {
+        public SVOfficeDbContext(DbContextOptions<SVOfficeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
