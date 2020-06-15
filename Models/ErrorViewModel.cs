6e2220df0e17f852a269a78aec7ae3ab1c503a93
Sergiu Voloc <SergiuVoloc@users.noreply.github.com>
2020-06-15 20:58:35 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SVOffice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
